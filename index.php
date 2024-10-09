cb0a63c292320e4ebd5ffb47ca44807731e8c1c1
theguffs <youssefabdelmalak01@gmail.com>
2024-10-09 13:07:35 +0200

    metodo, creazione oggetti e stampa con echo

--- index.php
+++ index.php
@@ -13,8 +13,20 @@
             $this->year = $year;
             $this->genre = $genre;
         }
-      
-}
-    
+          // metodo info sul film
+        public function getMovieInfo() {
+            return "Title: " . $this->title . "<br>" .
+                "Director: " . $this->director . "<br>" .
+                "Year: " . $this->year . "<br>" .
+                "Genre: " . $this->genre . "<br><br>";
+        }
+    }
 
+    // creazione oggetti
+    $movie1 = new Movie("Inception", "Christopher Nolan", 2010, "Science Fiction");
+    $movie2 = new Movie("The Godfather", "Francis Ford Coppola", 1972, "Crime");
+
+    // echo film
+    echo $movie1->getMovieInfo();
+    echo $movie2->getMovieInfo();
 ?>
