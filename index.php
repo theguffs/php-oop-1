3beb61810748549b993559f9c1da3955b0ed6af8
theguffs <youssefabdelmalak01@gmail.com>
2024-10-09 13:02:09 +0200

    istanze e costruttore

--- index.php
+++ index.php
@@ -1,5 +1,20 @@
 <?php
     class Movie {
+        // istanze
+        public $title;
+        public $director;
+        public $year;
+        public $genre;
+        
+        // costruttore
+        public function __construct($title, $director, $year, $genre) {
+            $this->title = $title;
+            $this->director = $director;
+            $this->year = $year;
+            $this->genre = $genre;
+        }
+      
+}
+    
 
-    }
 ?>
